e25de9d021025ea8fa1e9e1c47c51157875dcb9c
sinisa <miskovic.sinisa44@gmail.com>
2019-12-12 14:04:11 +0100

    API update

--- app/Api.php
+++ app/Api.php
@@ -57,7 +57,7 @@ class Api {
     }
 
     public static function generate_data( $data ) {
-
+   // return $data->image_1;
     $array = [
                 'name'              => $data->name,
                 'type'              => $data->type,
@@ -66,21 +66,46 @@ class Api {
                 'short_description' => $data->short_description,
             ];
 
-        if( isset( $data->image_1 ) ) {
-            array_push( $array, [['src' => $data->image_1]] );
 
-            $array['images'] = $array['0'];
-            unset( $array[0] );
-        }
-        elseif( isset( $data->image_1 ) || isset( $data->image_1 ) ) {
+        if( isset( $data->image_1 ) && isset( $data->image_2 ) ) {
             array_push( $array, [['src' => $data->image_1 ]] );
 
             $array['images'] = $array['0'];
             unset( $array[0] );
 
             array_push( $array['images'], ['src'=> $data->image_2] );
+
+        }elseif( isset( $data->image_1 ) ) {
+            array_push( $array, [['src' => $data->image_1]] );
+
+            $array['images'] = $array['0'];
+            unset( $array[0] );
         }
 
+        if( isset( $data->category_1) && isset( $data->category_2 ) ) {
+            array_push( $array, [['id' => $data->category_1]]) ;
+
+            if( ! isset( $data->image_1) && ! isset( $data->image_2 ) ) {
+                $array['category'] = $array['0'];
+                unset( $array[0] );
+            }else{
+                $array['category'] = $array['1'];
+                unset( $array[1] );
+            }
+
+            array_push( $array['category'], ['id' => $data->category_2] );
+        }elseif( isset( $data->category_1 ) ) {
+            array_push( $array, [['id' => $data->category_id]] );
+
+            if( ! isset( $data->image_1) && ! isset( $data->image_2 ) ) {
+                $array['category'] = $array['0'];
+                unset( $array[0] );
+            }else{
+                $array['category'] = $array['1'];
+                unset( $array[1] );
+            }
+
+        }
         return $array;
     }
 }
